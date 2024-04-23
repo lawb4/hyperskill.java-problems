f4fbd7464b3ad13748beb0b84d4965cefe538f57
Yahor_Labanau <yahorlabanau@gmail.com>
2024-04-23 13:41:55 +0300

    add more solved tasks

---
+++ Topics/For-loop/Fizz Buzz/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+
+        for (int i = start; i <= end; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+                continue;
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+                continue;
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
