89a60f374e591bfc91fb3c99d65ca734a9f96b2e
Yahor_Labanau <yahorlabanau@gmail.com>
2024-05-07 16:07:03 +0300

    add more solved tasks

---
+++ Topics/Reading user's input with Scanner/Print strings in the reverse order/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+        String str3 = scanner.nextLine();
+
+        System.out.printf("%s\n%s\n%s", str3, str2, str1);
+    }
+}
